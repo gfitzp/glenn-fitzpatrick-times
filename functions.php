24ae2ec3ab61623e7ed68d4856b0bdd48174de75
Glenn Fitzpatrick <glenn@glennfitzpatrick.com>
2016-04-25 19:28:12 -0400

    Hide the reCAPTCHA field on 2FA login
    Using Two Factor Authentication (https://www.simbahosting.co.uk/s3/product/two-factor-authentication/) and Google Captcha (reCAPTCHA) by BestWebSoft (http://bestwebsoft.com/products/) plugins, reCAPTCHA was still visible on 2FA login screen. Created CSS to hide reCAPTCHA field when username/password fields were also hidden.

--- functions.php
+++ functions.php
@@ -1,3 +1,8 @@
 <?php
 
 $content_width = 1920; /* pixels */
+
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );

af94c7d2b9ce8e7b1528e8cfb9884b3f1c0a3c07
Glenn Fitzpatrick <glenn@glennfitzpatrick.com>
2016-04-25 22:43:39 -0400

    Unprotect _member_access_role key
    WP protects custom fields beginning with an underscore; Members plugin uses _member_access_role to indicate which role can view a post. Needed to specifically unprotect the key in order to be able to modify keyvalue in MarsEdit.

--- functions.php
+++ functions.php
@@ -1,8 +1,27 @@
 <?php
 
+
+/* Single-image content width = 960px; set content_width to 2x for retina-quality graphics */
+
 $content_width = 1920; /* pixels */
+
+
+/* Hide reCAPTCHA on 2FA login screen */
 
 function my_login_stylesheet() {
     wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+
+/* Enable (unprotect) member role as a custom field for entering in MarsEdit or other posting apps
+   (custom fields beginning with an underscore are protected by WP */
+
+add_filter( 'is_protected_meta', function( $protected, $meta_key, $meta_type )
+{
+    $allowed = array( '_members_access_role' );
+    if( in_array( $meta_key, $allowed ) )
+        return false;
+
+    return $protected;
+}, 10, 3 );
